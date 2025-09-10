1ffc24948b3835908e0a785fe12e579e51463877
shreyan badyal <badyalshreyan@gmail.com>
2025-09-10 11:13:05 +0530

    Implemented Interview generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import react from "react";
+import Image from "next/image";
+import IntervViewAgent from "@/components/InterviewAgent";
+const page = () => {
+    return (
+        <div>
+            <p>Draft Your Interview here</p>
+            <IntervViewAgent />
+        </div>
+    )
+}
+export default page;
